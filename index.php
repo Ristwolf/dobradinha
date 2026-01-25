37abdba6dddf37a53c68d2d51a43897d625d842c
Andre Ristow <andre.ristow@proponto.com.br>
2026-01-24 23:03:30 -0300

    Set page titles to Instituto Hack

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ function formatDatePtBr($dateIso)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jonathan Hack | Pastor & Autor & Teólogo</title>
+    <title>Instituto Hack</title>
     <link rel="icon" type="image/x-icon" href="/static/favicon.ico">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
